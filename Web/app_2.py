d1a15e1da24833029d5b8244fad7e0d8ffed4c65
Greg Smith <33034302+greg-smith1@users.noreply.github.com>
2018-05-29 16:15:45 -0400

    Updated HTML and flask app to those on production

--- Web/app_2.py
+++ Web/app_2.py
@@ -1,4 +1,3 @@
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -28,6 +27,8 @@ def tech():
 def author():
     return render_template('author.html')
 
+app_dash.title='NHL Projections'
+
 colors = {
     'background': '#F3F5D8',
     'text': '#1E567C'
@@ -35,9 +36,12 @@ colors = {
 
 df = pd.read_csv('https://raw.githubusercontent.com/greg-smith1/proj_1/master/all_current_players.csv')
 
+app_dash.css.append_css({'external_url': 'https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css'})
+app_dash.css.append_css({'external_url': 'https://getbootstrap.com/docs/4.0/examples/sticky-footer-navbar/sticky-footer-navbar.css'})
+
 teams = sorted(df['Team'].unique())
 
-team_names = {'BOS':'Boston Bruins', 'CAR':'Carolina Hurricanes', 'DET':'Detroit Red Wings', 
+team_names = {'BOS':'Boston Bruins', 'CAR':'Carolina Hurricanes', 'DET':'Detroit Red Wings',
               'WPG':'Winnipeg Jets', 'BUF':'Buffalo Sabres', 'FLA':'Florida Panthers',
               'MTL':'Montreal Canadiens', 'OTT':'Ottawa Senators', 'TBL':'Tampa Bay Lightning',
               'TOR':'Toronto Maple Leafs', 'CBJ':'Columbus Blue Jackets', 'NJD':'New Jersey Devils',
@@ -52,7 +56,7 @@ team_names = {'BOS':'Boston Bruins', 'CAR':'Carolina Hurricanes', 'DET':'Detroit
 app_dash.layout = html.Div(style={}, children=[
     
     html.Span(html.A(html.Button('Homepage', className='homepage', style={
-    'backgroundColor': '#1D2125',
+    'backgroundColor': '#272C31',
     'border': 'none',
     'color': 'white',
     'textAlign': 'center',
@@ -60,16 +64,9 @@ app_dash.layout = html.Div(style={}, children=[
     'display': 'block',
     'fontSize': '20px'}),
     href='/'),
-    style={'backgroundColor': '#1D2125', 'padding': '16px', 'display':'block'}
+    style={'backgroundColor': '#272C31', 'padding': '10px', 'display':'block'}
     ),
 
-#    html.H1(
-#        children='NHL Goals by Season',
-#        style={
-#            'textAlign': 'center',
-#            'color':colors['text']
-#        }
-#    ),
   
     html.Div(id='output-a',
         style={
@@ -84,23 +81,20 @@ app_dash.layout = html.Div(style={}, children=[
         dcc.Dropdown(
             id='xaxis-column',
             options=[{'label': i, 'value': i} for i in teams],
-            value='DET'
+            value='DET',
+            clearable=False
             ),
         ],
         style={'width': '15%', 'margin':'auto', 'display':'block'}),
 
+        html.P(children='Click and drag to select/zoom, double click to reset view',
+        style={'textAlign': 'center', 'margin-top':'20'}
+    ),
 
 
-    dcc.Graph(id='nhl_goals'),
 
-#    dcc.Slider(
-#        id='year-slider',
-#        min=df['Season'].min(),
-#        max=df['Season'].max(),
-#        value=df['Season'].min(),
-#        step=None,
-#        marks={str(year): str(year) for year in df['Season'].unique()}
-#    )
+    dcc.Graph(id='nhl_goals', config={"displayModeBar": False}),
+
 ])
 
 
@@ -117,9 +111,9 @@ def update_figure(selected_team):
             y=df_by_player['G'],
             text=df_by_player['Team'],
             mode='lines+markers',
-            opacity=0.7,
+            opacity=0.8,
             marker={
-                'size': 6,
+                'size': 4.5,
                 'line': {'width': 0.5, 'color': 'white'}
             },
             name=i
@@ -128,9 +122,9 @@ def update_figure(selected_team):
     return {
         'data': traces,
         'layout': go.Layout(
-            xaxis={'type': 'linear', 'title': 'Season'},
+            xaxis={'type': 'linear', 'title': 'Season', 'autotick': 'False'},
             yaxis={'title': 'Goals'},
-            margin={'l': 90, 'b': 40, 't': 40, 'r': 90},
+            margin={'l': 100, 'b': 40, 't': 23, 'r': 100},
             #legend={'x': 0, 'y': 1},
             hovermode='compare'
         )
@@ -146,3 +140,5 @@ def callback_a(dropdown_value):
 
 if __name__ == '__main__':
     app_flask.run(host='0.0.0.0', debug=True)
+
+
